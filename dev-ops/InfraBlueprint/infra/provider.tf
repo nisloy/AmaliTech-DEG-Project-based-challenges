ee2f48cb9361871dc1212a92891b9cd91ae650cc
nisloy <nisloyamuza@gmail.com>
2026-04-26 15:32:03 +0200

    implemented  Outputs and S3 Remote State Backend

--- dev-ops/InfraBlueprint/infra/provider.tf
+++ dev-ops/InfraBlueprint/infra/provider.tf
@@ -11,6 +11,14 @@ terraform {
       version = "~> 5.0"
     }
   }
+  
+  backend "s3" {
+    bucket         = "vela-terraform-state-YOUR_SUFFIX" # REPLACE with your unique bucket name
+    key            = "infra/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "vela-terraform-locks" # REPLACE with your DynamoDB table name (Partition key: LockID)
+  }
 }
 
 provider "aws" {
